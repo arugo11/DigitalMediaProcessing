1fe1a6c36b9941e1bcbd068c82ab8bae3ecbcc6f
Argo11 <al23088@shibaura-it.ac.jp>
2024-12-16 02:09:30 +0900

    feat:hogefuga実装

--- exer02.py
+++ exer02.py
@@ -10,4 +10,7 @@ N = int(sys.argv[1])
 
 # !!このfor文の中を編集!!
 for i in range(1,N+1):
-    print(i)
+    if (i % 3 == 0) and (i % 5 == 0):print("hogefuga")
+    elif i % 3 == 0:print("hoge")
+    elif i % 5 == 0: print("fuga")
+    else: print(i)
