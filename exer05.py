63db0cb822f4ac1939c4c580cd8594652f7e6b8a
Argo11 <al23088@shibaura-it.ac.jp>
2024-12-18 05:44:00 +0900

    feat: Q-tableを用いた簡単な強化学習の実装を追加

--- exer05.py
+++ exer05.py
@@ -3,3 +3,70 @@
 # これより上は編集しないこと
 
 #雛形特になし、自由に書いてみてください
+"""
+Q-tableを用いた簡単な強化学習
+"""
+import numpy as np
+import cv2
+import sys
+
+# 行動の定義
+UP   :int = 0
+RIGHT:int = 1
+DOWN :int = 2
+LEFT :int = 3
+
+# 配列と迷路の状況の対応付け
+WALL :np.ndarray = np.array([0, 0, 0])
+AISLE:np.ndarray = np.array([255, 255, 255])
+START:np.ndarray = np.array([255, 0 , 0])
+GOAL :np.ndarray = np.array([0, 0, 255])
+
+class Env:
+    def __init__(self):
+        self._maze = np.uint8( cv2.imread(sys.argv[1]) )
+        self.reset()
+
+    # 盤面の初期化
+    def reset(self):
+
+
+        # スタートポジション
+        #NOTE: なんかこの書き方よくないらしい
+        self._state = list(map(int,np.where(np.all(self._maze == START, axis=-1))))
+        return np.array(self._state)
+    
+    # 行動
+    def step(self, action: int) -> tuple[np.ndarray, int]:
+        reward: int = 0
+
+        if action == UP: #up
+            self._state[0] -= 1
+        elif action == RIGHT:
+            self._state[1] += 1
+        elif action == DOWN:
+            self._state[0] -= 1
+        elif action == LEFT:
+            self._state[1] -= 1
+        else:
+            exit("予期しないアクション")
+
+        tile_statue: np.ndarray = self._maze[self._state[0], self._state[1]] #type:ignore
+
+        if np.all(tile_statue == WALL):
+            reward -= 1
+        elif np.all(tile_statue == AISLE):
+            reward += 0
+        elif np.all(tile_statue == GOAL):
+            reward += 1
+        return np.array(self._state,), reward
+
+
+
+
+def main() -> None:
+    env = Env()
+    print(env._state)
+
+if __name__ == "__main__":
+    main()
