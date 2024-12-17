a4387f254376d8013da0c09946c7240c99ac88c6
Argo11 <al23088@shibaura-it.ac.jp>
2024-12-18 05:41:37 +0900

    fix: 画像のグレースケール変換を修正

--- exer04.py
+++ exer04.py
@@ -12,20 +12,18 @@ fname_out = sys.argv[2]
 img = np.float32( cv2.imread(fname_in) )
 
 #grayscale画像の作成
-H   = img.shape[0]
-W   = img.shape[1]
+H, W = img.shape[:2] 
 img_gray = np.zeros((H,W), dtype=np.float32) #型はnp.float32
-
 
 
 #!!ここを編集!!
 for y in range(H) :
     for x in range(W) :
         #以下のコードでは，画像のr値をimg_grayに代入している(色はBGR順)
-        brue  = img[y,x,0]
+        blue  = img[y,x,0]
         green = img[y,x,1]
         red   = img[y,x,2]
-        img_gray[y,x] = red
+        img_gray[y,x] = (red + green + blue) / 3
 
 
 #save image
@@ -37,3 +35,4 @@ cv2.imwrite(fname_out, np.uint8(img_gray) )
 #  そのため画像を一度float値にして，計算をしてからint8に戻しています．
 #　float型のnumpy arrayは画像としてセーブできないのでセーブ時にuint8に変換しなおす必用が有ります
 #  またcv2.imwrite は，カラー（3channel）とグレースケール(1channel)の配列に対して利用可能です．
+
