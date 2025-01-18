7f3f635e63049d29cb2d3be1af24bd53ffa1203f
arugo11 <al23088@shibaura-it.ac.jp>
2025-01-18 15:35:16 +0900

    fix: 型チェックを無視するコメントを追加し、グレースケール化処理を実装

--- exer06.py
+++ exer06.py
@@ -14,8 +14,8 @@ fname_out = sys.argv[3]
 #load image
 img = np.float32( cv2.imread(fname_in) )
 
-height   = img.shape[0]
-width    = img.shape[1]
+height   = img.shape[0] # type: ignore
+width    = img.shape[1] # type: ignore
 img_gray = np.zeros((height, width), dtype=np.float32)
 
 
@@ -23,9 +23,16 @@ img_gray = np.zeros((height, width), dtype=np.float32)
 #!!ここを編集!!
 #ヒント : 前課題のグレースケール化をそのまま使いつつ，
 #        画素値がthreshold以上なら255を代入し，そうでなければ0を代入するとよい
-
-
+for y in range(height) :
+    for x in range(width) :
+        #以下のコードでは，画像のr値をimg_grayに代入している(色はBGR順)
+        blue: int  = img[y,x,0] # type: ignore
+        green: int = img[y,x,1] # type: ignore
+        red: int   = img[y,x,2] # type: ignore
+        gray : int = ((red + green + blue) // 3)
+        if gray >= threshold: img_gray[y][x] = 255
+        else: img_gray[y][x] = 0
 
 
 #save image
-cv2.imwrite(fname_out, np.uint8(img_gray) )
+cv2.imwrite(fname_out, np.uint8(img_gray) )#type:ignore
