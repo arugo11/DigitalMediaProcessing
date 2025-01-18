bb49ab817844523f829415507059f99344cc1e23
arugo11 <al23088@shibaura-it.ac.jp>
2025-01-18 15:35:28 +0900

    feat: 素数判定関数を追加し、インデックス行列を生成して画像に反映

--- exer08.py
+++ exer08.py
@@ -15,8 +15,27 @@ img = np.zeros((img_size, img_size), dtype = np.uint8)
 
 
 #!!ここを編集
-# N = 100程度の入力であれば、速度は気にせず実装しても問題なく動作するはずです。
+# 素数判定関数
+def is_prime(n):
+    if isinstance(n, np.ndarray):
+        return np.array([is_prime(x) for x in n.flat]).reshape(n.shape)
+    if n < 2: return False
+    return not any(n % i == 0 for i in range(2, int(np.sqrt(n)) + 1))
 
+# インデックス行列の生成
+index_mat = np.zeros((img_size, img_size), dtype=int)
+num = 1
 
+# 左下から右上への斜めパターンで値を設定
+for sum_idx in range(2 * img_size - 1):
+    for i in range(img_size):
+        j = sum_idx - i
+        if 0 <= j < img_size:
+            index_mat[img_size - 1 - i][j] = num
+            num += 1
+
+# 素数の位置を白(255)に設定
+prime_positions = is_prime(index_mat)
+img[prime_positions] = 255
 #save image
 cv2.imwrite(fname_out, img )
