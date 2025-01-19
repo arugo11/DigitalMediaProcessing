86e84f43eccc15007d60335ac6add6e0bbcf7d4c
arugo11 <al23088@shibaura-it.ac.jp>
2025-01-19 20:17:43 +0900

    feat: ハフマン符号化の実装と出力ファイルの追加

--- exer14.py
+++ exer14.py
@@ -8,19 +8,65 @@ import sys
 import math
 
 class Node :
-    def __init__(self, symbol , value) :
-        self.symbol = symbol # このノードのシンボル（a~hのどれか）
-        self.value  = value  # このノードの持つ値（出現確率）
-        self.parent = None   # 親ノード（Noneで初期化）
-        self.left   = None   # 左の子（Noneで初期化）
-        self.right  = None   # 右の子（Noneで初期化）
+    def __init__(self, symbol , value)  -> None:
+        self.symbol:str|None = symbol # このノードのシンボル（a~hのどれか）
+        self.value:int  = value  # このノードの持つ値（出現確率）
+        self.parent:Node|None = None   # 親ノード（Noneで初期化）
+        self.left:Node|None   = None   # 左の子（Noneで初期化）
+        self.right:Node|None  = None   # 右の子（Noneで初期化）
 
 
 
 def main() -> None:
     with open(sys.argv[1]) as f:
-        s = f.read()
-        print(s)
+        s:str = f.read()
+    # print(s)
 
+
+    table: dict[str, int] = {key:0 for key in set(list(s))}
+
+    for i in s:
+        table[i] += 1
+
+    nodes: list[Node] = []
+    for symbol, value in table.items():
+        nodes.append(Node(symbol=symbol,value=value))
+
+    while len(nodes) > 1:
+        nodes.sort(key = lambda x:x.value)
+        # left_node < right_node
+        left_node:Node = nodes.pop(0)
+        right_node:Node = nodes.pop(0)
+
+        parent_node:Node = Node(
+            symbol=None,
+            value=(right_node.value + left_node.value)
+        )
+
+        parent_node.left = left_node
+        parent_node.right = right_node
+        left_node.parent = parent_node
+        right_node.parent = parent_node
+
+        nodes.append(parent_node)
+
+
+    def generate_codes(node: Node, code: str = "") -> None:
+        if node.symbol is not None:
+            huffman_coding[node.symbol] = code
+            return
+
+        if node.left:
+            generate_codes(node.left, code + "1")
+        if node.right:
+            generate_codes(node.right, code + "0")
+
+    huffman_coding:dict[str,str] = {}
+    root_node = nodes[0]
+    generate_codes(root_node)
+
+    with open(sys.argv[2], mode='w') as f:
+        for _, value  in sorted(huffman_coding.items()):
+            f.write(f"{value}\n")
 if __name__ == '__main__':
     main()
