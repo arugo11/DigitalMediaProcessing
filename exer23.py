6bc46aa81ccf524e7395c53fd1791763210d1b48
arugo11 <al23088@shibaura-it.ac.jp>
2025-01-22 11:58:07 +0900

    fix:フィルター関数を修正

--- exer23.py
+++ exer23.py
@@ -8,20 +8,7 @@ import numpy as np
 import sys
 import os
 
-def normalize(image, out_min=0, out_max=255):
-    img = image.astype(np.float32)
-    
 
-    in_min = np.min(img)
-    in_max = np.max(img)
-    
-
-    normalized = (img - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-    
-
-    result = np.clip(normalized, 0, 255).astype(np.uint8)
-    
-    return result
 
 def quarter_swap(image):
     """
@@ -33,22 +20,22 @@ def quarter_swap(image):
     h_bottom = h - h_top
     w_left = w // 2
     w_right = w - w_left
-    
+
     # 結果用
     result = np.zeros_like(image)
-    
+
     top_left = image[:h_top, :w_left]
     top_right = image[:h_top, w_left:]
     bottom_left = image[h_top:, :w_left]
     bottom_right = image[h_top:, w_left:]
-    
+
 
     result[:h_bottom, :w_right] = bottom_right    # 右下 → 左上
     result[:h_bottom, w_right:] = bottom_left     # 左下 → 右上
 
     result[h_bottom:, :w_right] = top_right      # 右上 → 左下
     result[h_bottom:, w_right:] = top_left       # 左上 → 右下
-    
+
     return result
 
 def deconvolution_simple(img_path, kernel_path):
@@ -78,11 +65,10 @@ def deconvolution_simple(img_path, kernel_path):
 
     F = H * IMG
     f = np.fft.ifft2(F)
-    # 虚数は無視する(井尻先生談)
     f = np.abs(f)
     f = f[:img.shape[0], :img.shape[1]]
 
-    deconvolved_img = quarter_swap(normalize(f))
+    deconvolved_img = quarter_swap(f)
 
 
     return deconvolved_img
@@ -109,15 +95,14 @@ def deconvolution_wiener(img_path, kernel_path, K=1e-7):
 
 
     # Wienerフィルタ
-    """H = (G* * |G|^2) / (|G|^4 + K)"""
-    H = (np.conj(KERNEL) * np.abs(KERNEL)**2) / (np.abs(KERNEL)**4 + K) #Kは1e-7がよさげ
+    H = (np.conj(KERNEL)) / (np.abs(KERNEL)**2 + K) #Kは1e-7がよさげ
 
     F = H * IMG
     f = np.fft.ifft2(F)
     f = np.abs(f)
     f = f[:img.shape[0], :img.shape[1]]
 
-    deconvolved_img = quarter_swap(normalize(f))
+    deconvolved_img = quarter_swap(f)
 
 
     return deconvolved_img
